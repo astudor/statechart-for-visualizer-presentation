c70d9d7bfd7693cd844beb8130f46742ef39a288
tdk <andrei.s.tudor@gmail.com>
2020-01-08 11:57:50 +0200

    statemachine for visualizer

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,59 @@
-export function run(str) {
-  alert(str);
-}
+import { Machine, interpret, assign, spawn, send } from "xstate";
+
+// copy following code into https://xstate.js.org/viz/ statechart panel
+// to generate interactive chart
+export const sc = {
+  id: "ME PRESENTING",
+  initial: "monolog",
+  states: {
+    monolog: {
+      onDone: "dialog",
+      initial: "Explaining What Statechart Is",
+      states: {
+        "Explaining What Statechart Is": {
+          on: { FINISH: "Explaining Why To Use It" },
+          type: "parallel",
+          states: {
+            "Showing How Chart Looks": {},
+            "Offer Code Preview": {
+              initial: "Hiding How Code Looks",
+              states: {
+                "Hiding How Code Looks": {
+                  on: { TOGGLE: "Showing How Code Looks" }
+                },
+                "Showing How Code Looks": {
+                  on: { TOGGLE: "Hiding How Code Looks" }
+                }
+              }
+            }
+          }
+        },
+        "Explaining Why To Use It": {
+          initial: "Exemplifying Problem",
+          states: {
+            "Exemplifying Problem": {
+              on: { GET_CONCLUSION: "statingArguments" }
+            },
+            statingArguments: {
+              on: { SHOW_SOLUTION: "#solution" }
+            }
+          }
+        },
+        "Showing How To Use It": {
+          id: "solution",
+          on: {
+            END_SLIDES: "finalNote"
+          }
+        },
+        finalNote: { type: "final" }
+      }
+    },
+    dialog: {
+      after: {
+        3000: "Saying Goodbye"
+      }
+    },
+    "Saying Goodbye": { type: "final" }
+  }
+};
+const statechartPresentation = Machine(sc);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import { run } from "./app/app";
+import { sc as statechartPresentation } from "./app/app";
 import "./main.css";
 
-run("app is working");
+console.log(statechartPresentation);
+document.write(
+  `<h1>You can make a state machine out of following code, at https://xstate.js.org/viz/</h1>
+  <code>
+    ${JSON.stringify(statechartPresentation)}
+  </code>`
+);
